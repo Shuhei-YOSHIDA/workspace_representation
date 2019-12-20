ae85abce2a4a7254c5240b372a4282491d3a4c29
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-12-20 21:35:55 +0900

    updated

--- include/workspace_representation/workspace_representation.h
+++ include/workspace_representation/workspace_representation.h
@@ -1,11 +1,13 @@
 /**
  * @file workspace_representation.h
+ * @note A part of this code is inspired by "ros/robot_state_publisher"
  */
 
 #ifndef INCLUDE_WORKSPACE_REPRESENTATION_WORKSPACE_REPRESENTATION_H
 #define INCLUDE_WORKSPACE_REPRESENTATION_WORKSPACE_REPRESENTATION_H
 
 #include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
 #include <urdf/model.h>
 #include <kdl/frames.hpp>
 #include <kdl/segment.hpp>
@@ -14,17 +16,36 @@
 namespace workspace_representation
 {
 
+class SegmentPair
+{
+public:
+  SegmentPair(const KDL::Segment& p_segment,
+              const std::string& p_root,
+              const std::string& p_tip)
+    : segment(p_segment), root(p_root), tip(p_tip)
+  {
+  }
+
+  KDL::Segment segment;
+  std::string root;
+  std::string tip;
+};
+
 class WorkspaceRepresentation
 {
-private:
-  std::string urdf_string_;
-
 public:
-  WorkspaceRepresentation(std::string urdf_string);
+  WorkspaceRepresentation(const KDL::Tree& tree, const urdf::Model& model);
   virtual ~WorkspaceRepresentation();
 
   /// Represent workspace by sampling configuration randomly
   void prepareWorkspace();
+
+private:
+  void addChildren(const KDL::SegmentMap::const_iterator segment);
+
+  //std::string urdf_string_;
+  urdf::Model model_;
+  std::map<std::string, SegmentPair> segments_, segments_fixed_;
 };
 
 }
--- src/workspace_representation/workspace_representation.cpp
+++ src/workspace_representation/workspace_representation.cpp
@@ -3,17 +3,56 @@
  */
 
 #include "workspace_representation/workspace_representation.h"
+#include <kdl/frames_io.hpp>
 
 using namespace std;
+using namespace sensor_msgs;
 using namespace workspace_representation;
 
-WorkspaceRepresentation::WorkspaceRepresentation(string urdf_string)
-  : urdf_string_(urdf_string)
+WorkspaceRepresentation::WorkspaceRepresentation(const KDL::Tree& tree, const urdf::Model& model)
+  : model_(model)
 {
+  // walk the tree and add segment to segments_
+  addChildren(tree.getRootSegment());
 
 }
 
 void WorkspaceRepresentation::prepareWorkspace()
 {
 
+}
+
+// add children to correct maps
+void WorkspaceRepresentation::addChildren(const KDL::SegmentMap::const_iterator segment)
+{
+  const string& root = GetTreeElementSegment(segment->second).getName();
+
+  auto children = GetTreeElementChildren(segment->second);
+  for (int i = 0; i < children.size(); i++)
+  {
+    auto child = GetTreeElementSegment(children[i]->second);
+    SegmentPair s(GetTreeElementSegment(children[i]->second), root, child.getName());
+    string child_joint_name = child.getJoint().getName();
+    if (child.getJoint().getType() == KDL::Joint::None)
+    {
+      if (model_.getJoint(child_joint_name) &&
+          model_.getJoint(child_joint_name)->type == urdf::Joint::FLOATING)
+      {
+        ROS_INFO_STREAM("FLoating joint. Not adding segment from "
+            << root << " to " << child.getName()
+            <<  ". This joint is not used based on joint_states info");
+      }
+      else
+      {
+        segments_fixed_.insert(make_pair(child_joint_name, s));
+        ROS_DEBUG("Adding fixed segment from %s, to %s", root.c_str(), child.getName().c_str());
+      }
+    }
+    else
+    {
+      segments_.insert(make_pair(child_joint_name, s));
+      ROS_DEBUG("Adding moveing segment from %s to %s", root.c_str(), child.getName().c_str());
+    }
+    addChildren(children[i]);
+  }
 }
